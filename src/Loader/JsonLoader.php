3becc5d59707ba9e36f8389cc884c7645ae05791
Fred Blanc <findel@free.fr>
2021-03-14 23:58:17 +0100

    Improve Liskov substitution example

--- src/Loader/JsonLoader.php
+++ src/Loader/JsonLoader.php
@@ -4,10 +4,29 @@ declare(strict_types=1);
 
 namespace solid\Loader;
 
+use DateTimeImmutable;
+
 class JsonLoader extends CsvLoader
 {
-    public function load(string $filename): array
+    private function loadFile(string $filename): array
     {
         return json_decode(file_get_contents($filename), true);
+    }
+
+    public function load(string $filename): array
+    {
+        $content = $this->loadFile($filename);
+
+        return array_map(
+            fn ($value) => array_values($value),
+            $content['users']
+        );
+    }
+
+    public function getDate(string $filename): DateTimeImmutable
+    {
+        $content = $this->loadFile($filename);
+
+        return new DateTimeImmutable($content['updatedAt']);
     }
 }
